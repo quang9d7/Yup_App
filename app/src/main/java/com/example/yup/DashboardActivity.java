fef6b561224eb7cbd0dec6d2a5ec3b5f99213efb
ngotruonghuy <truonghuycorn@gmail.com>
2022-06-12 04:17:40 +0700

    Intent read image and function draw box on image

--- app/src/main/java/com/example/yup/DashboardActivity.java
+++ app/src/main/java/com/example/yup/DashboardActivity.java
@@ -1,14 +1,103 @@
 package com.example.yup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.yup.adapters.ImageAdapter;
+import com.example.yup.adapters.UserAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ArrayList<Uri> images = new ArrayList<>();
+    RecyclerView imageCollection;
+    FloatingActionButton pickImageButton;
+    ImageAdapter imageAdapter;
+
+    private static final int READ_PERMISSION = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_dashboard_temp);
+
+        pickImageButton = findViewById(R.id.fab);
+
+        imageCollection = findViewById(R.id.imageCollection);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        imageCollection.setLayoutManager(layoutManager);
+        imageAdapter = new ImageAdapter(images,getWindowManager());
+        imageCollection.setAdapter(imageAdapter);
+
+        if (ContextCompat.checkSelfPermission
+                (DashboardActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(DashboardActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_PERMISSION);
+        }
+
+        pickImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select images"),1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (data.getClipData() != null) {
+                int x = data.getClipData().getItemCount();
+                for (int i=0; i<x; i++) {
+                    String url = data.getClipData().getItemAt(i).getUri().getPath();
+                    images.add(Uri.parse(url));
+                    imageAdapter.notifyDataSetChanged();
+                }
+
+            }
+            else if (data.getData() != null) {
+                String imageUrl = data.getData().getPath();
+                images.add(Uri.parse(imageUrl));
+                imageAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+    private Bitmap drawRectOnBitMap(Bitmap bm, float left, float top, float right, float bottom){
+        Bitmap bm1 = bm.copy(bm.getConfig(),true);
+        Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStrokeWidth(30);
+        mPaint.setStyle(Paint.Style.STROKE);
+        Canvas canvas = new Canvas(bm1);
+        canvas.drawRect(left,top,right,bottom,mPaint);
+
+        return bm1;
     }
 }
