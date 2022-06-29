9aa2b9fb3bc2ef01170edcc6c917eb5889b21207
Cong Thanh Vu <congthanh3181@gmail.com>
2022-06-30 00:13:07 +0700

    Added share function

--- app/src/main/java/com/example/yup/EditorActivity.java
+++ app/src/main/java/com/example/yup/EditorActivity.java
@@ -21,6 +21,7 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.core.content.FileProvider;
 
 import com.example.yup.models.DownloadImage;
+import com.example.yup.models.InfoMessage;
 import com.example.yup.models.MyDetectImage;
 import com.example.yup.models.MyDetectInfo;
 import com.example.yup.utils.ApiService;
@@ -44,6 +45,7 @@ import retrofit2.Response;
 public class EditorActivity extends AppCompatActivity {
     AppCompatImageButton saveImageBtn;
     AppCompatImageButton shareBtn;
+    AppCompatImageButton deleteBtn;
     ImageDrawView imageDrawView;
     ProgressBar progressBar;
     ApiService service;
@@ -63,19 +65,20 @@ public class EditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         Intent intent = getIntent();
-        String detect_id = intent.getStringExtra("image_id");
+        String image_id = intent.getStringExtra("image_id");
 
         sharedPreferences = getSharedPreferences(getResources().getString(R.string.yup_sp), MODE_PRIVATE);
         sessionManager = SessionManager.getInstance(sharedPreferences);
         service = Client.createServiceWithAuth(ApiService.class, sessionManager);
 
-        Log.d("detect_id in temp Activity", detect_id);
+        Log.d("image_id in temp Activity", image_id);
         imageDrawView = findViewById(R.id.mainImage);
         saveImageBtn = findViewById(R.id.saveImageBtn);
         shareBtn = findViewById(R.id.shareBtn);
+        deleteBtn = findViewById(R.id.deleteBtn);
         progressBar = findViewById(R.id.progressBar);
         showProgressBar();
-        Call<MyDetectImage> call_detect = service.getDetectId(detect_id);
+        Call<MyDetectImage> call_detect = service.getDetectId(image_id);
         call_detect.enqueue(new Callback<MyDetectImage>() {
             @Override
             public void onResponse(Call<MyDetectImage> call, Response<MyDetectImage> response) {
@@ -126,31 +129,50 @@ public class EditorActivity extends AppCompatActivity {
         saveImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-                mPaint.setColor(Color.BLUE);
-                mPaint.setStrokeWidth(5);
-                mPaint.setStyle(Paint.Style.STROKE);
-                Bitmap processedBitmap = imageDrawView.getFullSizeBitmap(mPaint);
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                String imageFileName = "JPEG_" + timeStamp + "_";
-                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-                try {
-                    File image = File.createTempFile(
-                            imageFileName,  /* prefix */
-                            ".jpg",         /* suffix */
-                            storageDir      /* directory */
-                    );
-                    FileOutputStream fOut = new FileOutputStream(image);
-                    processedBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
-                    fOut.flush();
-                    fOut.close();
+                if (saveFile() != null)
                     // Notify that the image saved successfully
                     Toast.makeText(EditorActivity.this, "Đã lưu ảnh thành công", Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         });
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = saveFile();
+                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_STREAM, uri);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setType("image/jpeg");
+                startActivity(intent);
+            }
+        });
+    }
+    Uri saveFile() {
+        Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStrokeWidth(5);
+        mPaint.setStyle(Paint.Style.STROKE);
+        Bitmap processedBitmap = imageDrawView.getFullSizeBitmap(mPaint);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try {
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+            FileOutputStream fOut = new FileOutputStream(image);
+            processedBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+            return FileProvider.getUriForFile(
+                    EditorActivity.this,
+                    "com.example.yup.provider",
+                    image);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
     List<List<Point>> transformToPoints(List<List<List<Float>>> raw) {
         List<List<Point>> boxes = new ArrayList<>();
